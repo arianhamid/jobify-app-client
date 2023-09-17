360e761af6d3df7b62b47a466c293dcd8364bae3
Hamid <hamidarianyegane@gmail.com>
2023-09-17 13:03:35 +0330

    create pages

---
+++ src/pages/AddJob.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AddJob = () => {
+  return <div>Addjob</div>;
+};
+
+export default AddJob;
---
+++ src/pages/HomeLayout.jsx
@@ -0,0 +1,10 @@
+
+const HomeLayout = () => {
+  return (
+    <h1>
+      HomeLayout
+    </h1>
+  )
+}
+
+export default HomeLayout
