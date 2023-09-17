b881a5d621e9c160c2efa1d4ebbb9195026570c2
Hamid <hamidarianyegane@gmail.com>
2023-09-17 21:31:13 +0330

    Logo component added

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,24 +1,34 @@
 import Wrapper from "../assets/wrappers/LandingPage";
 import main from "../assets/images/main.svg";
-import logo from "../assets/images/logo.svg";
 import { Link } from "react-router-dom";
+import { Logo } from "../components";
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt="jobify" className="logo" />
+        <Logo />
       </nav>
       <div className="container page">
         <div className="info">
-          <h1>job <span>tracking</span> app</h1>
-          <p>I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue bottle single-origin coffee chia. Aesthetic post-ironic venmo, quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch narwhal.</p>
-          <Link className="btn register-link" to="/register">Register</Link>
-          <Link className="btn " to="/login">Login / Demo User</Link>
-          </div>
-          <img src={main} alt="job hunt" className="img main-img" />
-
-          </div>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
+            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
+            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
+            narwhal.
+          </p>
+          <Link className="btn register-link" to="/register">
+            Register
+          </Link>
+          <Link className="btn " to="/login">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
     </Wrapper>
   );
 };
