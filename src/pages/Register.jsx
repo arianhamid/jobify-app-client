2debb316f2e69b2fb6a3c7e0d105ec02ba624fe7
Hamid <hamidarianyegane@gmail.com>
2023-09-18 12:36:52 +0330

    register component updated

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
-import { Logo } from "../components";
+import { FormRow, Logo } from "../components";
 
 const Register = () => {
   return (
@@ -8,71 +8,33 @@ const Register = () => {
       <form className="form">
         <Logo />
         <h4>Register</h4>
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">
-            name
-          </label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            className="form-input"
-            required={true}
-            // value=""
-          />
-        </div>
-        <div className="form-row">
-          <label htmlFor="lastName" className="form-label">
-            last name
-          </label>
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            className="form-input"
-            required={true}
-            // value=""
-          />
-        </div>
-        <div className="form-row">
-          <label htmlFor="location" className="form-label">
-            location
-          </label>
-          <input
-            type="text"
-            id="location"
-            name="location"
-            className="form-input"
-            required={true}
-            // value=""
-          />
-        </div>
-        <div className="form-row">
-          <label htmlFor="email" className="form-label">
-            email
-          </label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            className="form-input"
-            required={true}
-            // value=""
-          />
-        </div>
-        <div className="form-row">
-          <label htmlFor="password" className="form-label">
-            password
-          </label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            className="form-input"
-            required={true}
-            // value=""
-          />
-        </div>
+        <FormRow
+          type="text"
+          name="name"
+          defaultValue="hamid"
+        />
+        <FormRow
+          type="text"
+          name="lastName"
+          defaultValue="arian"
+          labelText="last name"
+        />
+        <FormRow
+          type="text"
+          name="location"
+          defaultValue="iran"
+        />
+        <FormRow
+          type="email"
+          name="email"
+          defaultValue="hamid@arian.com"
+        />
+        <FormRow
+          type="password"
+          name="password"
+          defaultValue="123456"
+        />
+
         <button type="submit" className="btn btn-block undefined ">
           submit
         </button>
